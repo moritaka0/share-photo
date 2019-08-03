7e644b271907e304f271b4a4e215a91fd5d78cb2
Takahiro Mori <moritaka0612@gmail.com>
2019-08-03 17:54:57 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
 //Composerでインストールしたライブラリの一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
-//POSTメソッドで渡される値を取得、表示
-$inputString = file_get_contents('php://input');
-error_log($inputString)
+
+//アクセストークンを使用したCurlHTTPClienをインスタンス化
+$httpClient = new ¥LINE¥LINEBot¥HTTPClient¥CurlHTTPCleint(getenv('CHANNEL_ACCESS_TOKEN'));
+
+//CurlHTTPClienとシークレットキーでLineBotをインスタンス化
+$bot = new ¥LINE¥LineBot($httpClient,['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+//LINE APIがリクエストに付与した署名を取得
+$signature = $_SERVER['HTTP_' . ¥LINE¥LineBot¥Constant¥HTTPHeader::LINE_SIGNATURE];
+
+//署名が正当かチェックし問題なければリクエストをパースして配列格納
+$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
+
+//配列に格納された各イベントをループで取得
+foreach($events as $event) {
+    $bot->replyText($event->getReplyToken(),'テスト返信')
+}
 
 
 ?>
