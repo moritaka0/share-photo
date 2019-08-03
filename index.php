c1a5db84e00a3356366469ddbc25742e1e026103
Takahiro Mori <moritaka0612@gmail.com>
2019-08-03 18:01:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 //アクセストークンを使用したCurlHTTPClienをインスタンス化
-$httpClient = new ¥LINE¥LINEBot¥HTTPClient¥CurlHTTPCleint(getenv('CHANNEL_ACCESS_TOKEN'));
+$httpClient = new ¥LINE¥LINEBot¥HTTPClient¥CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
 //CurlHTTPClienとシークレットキーでLineBotをインスタンス化
 $bot = new ¥LINE¥LineBot($httpClient,['channelSecret' => getenv('CHANNEL_SECRET')]);
