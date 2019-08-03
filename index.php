80117ebfcd4464e722d1c690708bb275570395ae
Takahiro Mori <moritaka0612@gmail.com>
2019-08-03 17:29:41 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //Composerでインストールしたライブラリの一括読み込み
-require_once __DIR__ . '/vendor/authload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 //POSTメソッドで渡される値を取得、表示
 $inputString = file_get_contents('php://input');
 error_log($inputString)
