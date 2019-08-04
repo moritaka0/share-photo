da53c55d7fb1c496f24fb69ffa354b243a3b2a8f
Takahiro Mori <moritaka0612@gmail.com>
2019-08-04 10:39:58 +0900

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,37 @@ $events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
 
 //配列に格納された各イベントをループで取得
 foreach($events as $event) {
-    $bot->replyText($event->getReplyToken(),'テスト返信');
+    //画像メッセージであれば画像をサーバーに保存
+    if($event instanceof \LINE\LINEBot\Event\MessageEvent\ImageMEssage){
+        //イベントのコンテンツを取得
+        $content = $bot->getMessageContent($event->getMessageID());
+        //コンテンツヘッダーを取得
+        $headers = $content->getHeaders();
+        error_log(var_export($headers,true));
+        //画像保存先フォルダーパス
+        $directory_path = 'tmp';
+        //ファイル名
+        $filename = uniqid();
+        //コンテンツの種類を取得
+        $extension = explode('/', $headers['Content-Type'])[1];
+        //保存先フォルダーがなければ作成する
+        if(!file_exists($directory_path)){
+            //ディレクトリ作成
+            if(mkdir($directory_path,0777,true)){
+                chmod($directory_path,0777);
+            }
+        }
+        //保存先ディレクトリに画像を保存
+        file_put_contents($directory_path . '/' . $filename . '.' . $extension, $content->getRawBody());
+
+        //保存したファイルのURLを返信
+        replyTextMessage($bot, $event->getReplyToken(), 'http://' . $_SERVER['HTTP_HOST'] . '/' . $directory_path. '/' . $filename . '.' . $extension);
+
+    }
+    else{
+        $bot->replyText($event->getReplyToken(),'画像の送信をお願いします');
+    }
+    
 }
 
 
