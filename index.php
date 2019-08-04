414aa0bcbf5d3e433fb6371a6dae360c95fb35d4
Takahiro Mori <moritaka0612@gmail.com>
2019-08-04 10:05:09 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
 //CurlHTTPClienとシークレットキーでLineBotをインスタンス化
-$bot = new ¥LINE¥LineBot($httpClient,['channelSecret' => getenv('CHANNEL_SECRET')]);
+$bot = new \LINE\LineBot($httpClient,['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 //LINE APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LineBot\Constant\HTTPHeader::LINE_SIGNATURE];
