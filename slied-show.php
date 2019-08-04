1b5951596a747175ffc21ace1cedf86504613847
Takahiro Mori <moritaka0612@gmail.com>
2019-08-04 13:08:26 +0900

    Update slied-show.php

--- slied-show.php
+++ slied-show.php
@@ -8,13 +8,60 @@
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
         integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
         crossorigin="anonymous">
-  <title>●ページタイトル●</title>
+  <title>はっしーまい結婚式</title>
 </head>
 
 <body>
 
 <!-- ここにページ内容を記述 -->
-<h1><?php echo 'Hello World!'; ?></>
+<div class="container">        <!-- 全体を囲むコンテナ -->
+
+  <h1><?php echo 'スライドショーテスト'; ?></>
+
+  <div class="carousel slide" id="c1" data-ride="carousel">
+    
+    <!-- カルーセルの下に表示するインディケーター -->
+    <!--
+    <ol class="carousel-indicators"> 
+      <li data-target="#c1" data-slide-to="0" class="active"></li>
+      <li data-target="#c1" data-slide-to="1"></li>
+      <li data-target="#c1" data-slide-to="2"></li>
+      <li data-target="#c1" data-slide-to="3"></li>
+    </ol>
+    -->
+    <!-- カルーセル内の画像 -->
+    <p>
+    <?php
+    foreach(glob('tmp/*') as $file){
+        if(is_file($file)){
+            echo htmlspecialchars($file);
+        }
+    }
+    ?>
+    </p>
+    <!--
+    <div class="carousel-inner">
+      <div class="carousel-item active"><img src="img/photo1.jpg" class="d-block w-100"></div>
+      <div class="carousel-item"><img src="img/photo2.jpg" class="d-block w-100"></div>
+      <div class="carousel-item"><img src="img/photo3.jpg" class="d-block w-100"></div>
+      <div class="carousel-item"><img src="img/photo4.jpg" class="d-block w-100"></div>
+    </div>
+
+    <a href="#c1" class="carousel-control-prev" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+      <span class="sr-only">前の画像へ</span>
+    </a>
+    <a href="#c1" class="carousel-control-next" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+      <span class="sr-only">次の画像へ</span>
+    </a>
+  </div>
+  -->
+
+</div>        <!-- 全体を囲むコンテナ -->
+
+
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
